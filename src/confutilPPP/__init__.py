92506f7ca91b02c24f9174b6fd1539bfef62863a
Aurorax-own <15047150695@163.cpm>
2023-08-15 19:45:24 +0800

    __version__ = '1.3.0'

--- src/confutilPPP/__init__.py
+++ src/confutilPPP/__init__.py
@@ -1,11 +1,7 @@
 from .__confutil__ import confutil
-from .logPPP import *
 
 __all__ = ['__version__', 'check_config']
-__version__ = '1.2.2'
-
-Config(logging_level=INFO, logging_is_output_file=True, logging_is_output_sys_stdout=True,
-       logging_file='confutilPPP.log', )
+__version__ = '1.3.0'
 
 
 def check_config(_object=None, _filename='config'):
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import confutilPPP
 
+confutilPPP._LOGGING_FILE = 'log.txt'
+
 
 def test():
     CONFIG = {
         'test': 'test'
     }
+
     conf = confutilPPP.check_config(_object=CONFIG, _filename='config')
     print(conf)
 
