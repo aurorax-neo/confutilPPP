4d000c5971eba7edc060dfc10773c7fdf744a806
Aurorax-own <15047150695@163.cpm>
2023-08-15 19:50:47 +0800

    __version__ = '1.3.0'

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import confutilPPP
 
-confutilPPP._LOGGING_FILE = 'log.txt'
-
 
 def test():
+    # _object : dict, 包含配置对象
+    # _filename : str, 配置文件名 默认为 config
+    # return : dict, 配置字典
+    # 首次当前位置生成 *.yml 文件，之后直接读取
+    # *.yaml也可用
     CONFIG = {
         'test': 'test'
     }
-
     conf = confutilPPP.check_config(_object=CONFIG, _filename='config')
     print(conf)
 
